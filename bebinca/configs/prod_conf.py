1419bc5bd46525c428be2217aed945308f9c285b
wangxun <1013296326@qq.com>
2024-09-17 11:12:52 +0800

    部署

---
+++ bebinca/configs/prod_conf.py
@@ -0,0 +1,19 @@
+from bebinca.configs.base_conf import BaseSettings
+
+
+class DevSettings(BaseSettings):
+    env = 'dev'
+
+    # mysql
+    db_name = 'WantSun$bebinca'
+    db_port = 3306
+    db_host = 'WantSun.mysql.pythonanywhere-services.com'
+
+    db_pool_size = 1
+    db_max_overflow = 10
+
+    httpx_pool_size = 10
+    httpx_max_overflow = 100
+
+    stream_pool_size = 10
+    stream_max_overflow = 100
